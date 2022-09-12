95b1aeb36873f8cfdb2aeddcf6d4c4dc682b4686
Kamran <neo2enigma@gmail.com>
2022-09-12 19:52:47 +0430

    toggle action added

---
+++ script.js
@@ -0,0 +1,26 @@
+const priceElements = document.querySelectorAll('.price');
+const toggle = document.querySelector('.switch');
+
+const pricePlans = {
+    'monthly': [19.99, 24.99, 39.99],
+    'annual': [199.99, 249.99, 399.99]
+};
+
+
+
+function setPrice(plan = 'monthly') {
+    let i = 0;
+    priceElements.forEach(element => {
+        element.lastElementChild.innerHTML = pricePlans[plan][i];
+        // element.style.opacity = .5;
+        i++;
+    });
+
+
+}
+
+toggle.addEventListener('change', (e) => {
+    const plan = e.target.checked ? 'annual' : 'monthly';
+    setPrice(plan);
+
+});
