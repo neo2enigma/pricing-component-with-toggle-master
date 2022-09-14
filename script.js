3510d7cb572e012e81fc0cf38059e7fba4f38540
Kamran <neo2enigma@gmail.com>
2022-09-14 23:40:17 +0430

    Text animation

--- script.js
+++ script.js
@@ -6,13 +6,32 @@ const pricePlans = {
     'annual': [199.99, 249.99, 399.99]
 };
 
+function textOpacifier(element) {
+    let i = 0;
 
+    const interval = setInterval(() => {
+
+        element.style.opacity = i;
+
+        i += 0.05;
+
+        if (i > 1)
+            clearInterval(interval);
+
+    }, 30);
+}
 
 function setPrice(plan = 'monthly') {
     let i = 0;
+
     priceElements.forEach(element => {
+
+        element.lastElementChild.style.opacity = 0;
+
+        textOpacifier(element.lastElementChild);
+
         element.lastElementChild.innerHTML = pricePlans[plan][i];
-        // element.style.opacity = .5;
+
         i++;
     });
 
